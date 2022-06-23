76641a138701d91e90dc9962647ca657629786c9
MGCzI <mgczarnievicz@gmail.com>
2022-06-23 16:14:57 +0200

    Herock 1

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const spicedPg = require("spiced-pg");
 let USER_NAME, USER_PASSWORD;
 console.log("process.env.NODE_ENV", process.env.NODE_ENV);
 
-if (process.env.DATABASE_URL) {
+if (!process.env.DATABASE_URL) {
     // Bc we are deploding we need to define where to get the value.
     USER_NAME = require("./secrets").USER_NAME;
     USER_PASSWORD = require("./secrets").USER_PASSWORD;
